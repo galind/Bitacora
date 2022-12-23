e8a8f4028aa7e16d3dc116618c94a097abd9e05e
galind <guillem.yt1@gmail.com>
2022-12-23 14:34:46 +0100

    Added cog with commands to manage modules

---
+++ cogs/owner/owner.py
@@ -0,0 +1,59 @@
+from discord.ext import commands
+
+from bot import Bitacora
+
+
+class Owner(commands.Cog):
+    """Owner-only commands to manage the extensions."""
+
+    def __init__(self, bot: Bitacora):
+        self.bot = bot
+        self.delay = 10  # Seconds to wait to delete a message
+
+    async def cog_check(self, ctx: commands.Context) -> bool:
+        return await self.bot.is_owner(ctx.author)
+
+    @commands.command(hidden=True)
+    async def load(self, ctx: commands.Context, module: str):
+        """Loads a module."""
+        try:
+            await self.bot.load_extension(f'cogs.{module}')
+        except commands.ExtensionError as e:
+            await ctx.send(
+                f'{e.__class__.__name__}: {e}', delete_after=self.delay
+            )
+        else:
+            await ctx.send(
+                f'Module \'{module}\' loaded.', delete_after=self.delay
+            )
+        await ctx.message.delete(delay=self.delay)
+
+    @commands.command(hidden=True)
+    async def unload(self, ctx: commands.Context, module: str):
+        """Unloads a module."""
+        try:
+            await self.bot.unload_extension(f'cogs.{module}')
+        except commands.ExtensionError as e:
+            await ctx.send(
+                f'{e.__class__.__name__}: {e}', delete_after=self.delay
+            )
+        else:
+            await ctx.send(
+                f'Module \'{module}\' unloaded.', delete_after=self.delay
+            )
+        await ctx.message.delete(delay=self.delay)
+
+    @commands.command(hidden=True)
+    async def reload(self, ctx: commands.Context, module: str):
+        """Reloads a module."""
+        try:
+            await self.bot.reload_extension(f'cogs.{module}')
+        except commands.ExtensionError as e:
+            await ctx.send(
+                f'{e.__class__.__name__}: {e}', delete_after=self.delay
+            )
+        else:
+            await ctx.send(
+                f'Module \'{module}\' reloaded.', delete_after=self.delay
+            )
+        await ctx.message.delete(delay=self.delay)
