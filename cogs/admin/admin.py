abb76ed8f72944cddc70262e9c2abb2e688dc2ee
galind <guillem.yt1@gmail.com>
2022-12-23 21:39:17 +0100

    Added update cooldown code

--- cogs/admin/admin.py
+++ cogs/admin/admin.py
@@ -1,7 +1,9 @@
 from discord.ext import commands
 from discord import app_commands
 import discord
+import importlib
 
+from cogs.utils import mongo
 from bot import Bitacora
 
 
@@ -13,10 +15,20 @@ class Admin(commands.GroupCog, group_name='admin'):
     def __init__(self, bot: Bitacora):
         self.bot = bot
 
+    async def cog_load(self) -> None:
+        importlib.reload(mongo)
+
     @app_commands.command(name='cooldown')
     @app_commands.describe(seconds='Set it to 0 for no cooldown')
     async def cooldown(self, interaction: discord.Interaction, seconds: int):
         """Set the cooldown between reactions"""
+        guild = mongo.Guilds(interaction.guild_id)
+        try:
+            await guild.update_guild('cooldown', seconds)
+            content = f'Cooldown has been updated to {seconds} seconds'
+        except Exception:
+            content = 'Something has failed'
+        await interaction.response.send_message(content, ephemeral=True)
 
     @app_commands.command(name='emoji')
     async def emoji(self, interaction: discord.Interaction):
