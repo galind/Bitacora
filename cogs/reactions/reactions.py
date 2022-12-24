619c7f1637397c3ca893d45f0585193806a8320d
galind <guillem.yt1@gmail.com>
2022-12-24 15:01:55 +0100

    Added listeners

--- cogs/reactions/reactions.py
+++ cogs/reactions/reactions.py
@@ -1,10 +1,19 @@
 from discord.ext import commands
+import discord
 
 from bot import Bitacora
 
 
 class Reactions(commands.Cog):
-    """Users can give coins to others reacting or tipping"""
+    """Users can give coins to others by reacting to a message"""
 
     def __init__(self, bot: Bitacora):
         self.bot = bot
+
+    @commands.Cog.listener(name='on_raw_reaction_add')
+    async def reaction_add(self, payload: discord.RawReactionActionEvent):
+        pass
+
+    @commands.Cog.listener(name='on_raw_reaction_remove')
+    async def reaction_remove(self, payload: discord.RawReactionActionEvent):
+        pass
