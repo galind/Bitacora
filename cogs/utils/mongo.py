f2bb40f5aee498fd004855a0108e7e9f0daae489
galind <guillem.yt1@gmail.com>
2022-12-24 04:23:42 +0100

    Added function annotations

--- cogs/utils/mongo.py
+++ cogs/utils/mongo.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from bot import Bitacora
 
 bot = Bitacora()
@@ -9,19 +11,19 @@ class Guilds:
         self.guild = database[str(guild)]
         self.query = {'_id': 'config'}
 
-    async def new_guild(self):
+    async def new_guild(self) -> None:
         await self.guild.insert_one(self.query)
 
-    async def find_guild(self):
+    async def find_guild(self) -> Optional[dict]:
         return await self.guild.find_one(self.query)
 
-    async def check_guild(self):
+    async def check_guild(self) -> dict:
         guild_config = await self.find_guild()
         if not guild_config:
             await self.new_guild()
             guild_config = await self.find_guild()
         return guild_config
 
-    async def update_guild(self, key, value):
+    async def update_guild(self, key, value) -> None:
         set_query = {'$set': {key: value}}
         await self.guild.update_one(self.query, set_query, upsert=True)
