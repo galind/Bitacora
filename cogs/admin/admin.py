09907cf2a097713be18cc18b8946af9d098472ca
galind <guillem.yt1@gmail.com>
2022-12-24 02:03:19 +0100

    Added update emoji code

--- cogs/admin/admin.py
+++ cogs/admin/admin.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 from discord import app_commands
 import discord
+import asyncio
 import importlib
 
 from cogs.utils import mongo
@@ -33,3 +34,40 @@ class Admin(commands.GroupCog, group_name='admin'):
     @app_commands.command(name='emoji')
     async def emoji(self, interaction: discord.Interaction):
         """Choose the emoji that will act as currency"""
+        await interaction.response.defer(ephemeral=True)
+        embed = discord.Embed(
+            title='React with the emoji you would like to set',
+            description=(
+                'You can use a default emoji, like 🪙, 💯, etc... '
+                'or use a custom emoji from your server. You have '
+                '90 seconds before this message auto-deletes.'
+            ),
+            color=0xFF0000
+        )
+        message = await interaction.channel.send(embed=embed)
+
+        def check(reaction: discord.Reaction, user: discord.User):
+            if (
+                user.id == interaction.user.id and
+                message.id == reaction.message.id
+            ):
+                return reaction
+            return None
+
+        try:
+            reaction = await self.bot.wait_for(
+                'reaction_add', check=check, timeout=90.0
+            )
+        except asyncio.TimeoutError:
+            await interaction.followup.send('No reaction was received')
+            return await message.delete()
+
+        emoji = str(reaction[0])
+        guild = mongo.Guilds(interaction.guild_id)
+        try:
+            await guild.update_guild('emoji', emoji)
+            content = f'Emoji has been updated to {emoji}'
+        except Exception:
+            content = 'Something has failed'
+        await message.delete()
+        await interaction.followup.send(content)
