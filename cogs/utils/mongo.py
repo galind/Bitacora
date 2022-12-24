1c8a005d3755dedfc336722323b7d3a25d3f0a48
galind <guillem.yt1@gmail.com>
2022-12-24 15:01:31 +0100

    Added users class

--- cogs/utils/mongo.py
+++ cogs/utils/mongo.py
@@ -24,6 +24,29 @@ class Guilds:
             guild_settings = await self.find_guild()
         return guild_settings
 
-    async def update_guild(self, key, value) -> None:
-        set_query = {'$set': {key: value}}
+    async def update_guild(self, query: dict) -> None:
+        set_query = {'$set': query}
+        await self.guild.update_one(self.query, set_query, upsert=True)
+
+
+class Users:
+    def __init__(self, guild: int, user: int):
+        self.guild = database[str(guild)]
+        self.query = {'_id': user}
+
+    async def new_user(self) -> None:
+        await self.guild.insert_one(self.query)
+
+    async def find_user(self) -> Optional[dict]:
+        return await self.guild.find_one(self.query)
+
+    async def check_user(self) -> dict:
+        user_info = await self.find_user()
+        if not user_info:
+            await self.new_user()
+            user_info = await self.find_user()
+        return user_info
+
+    async def update_user(self, query: dict) -> None:
+        set_query = {'$set': query}
         await self.guild.update_one(self.query, set_query, upsert=True)
