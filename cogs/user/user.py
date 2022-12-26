b1a3cd69c8b181eb2187a99dbe89d1e20b69ad86
galind <guillem.yt1@gmail.com>
2022-12-26 02:01:48 +0100

    Added command to check balance

--- cogs/user/user.py
+++ cogs/user/user.py
@@ -1,8 +1,21 @@
 from discord.ext import commands
+from discord import app_commands
+import discord
 
+from cogs.utils import mongo
 from bot import Bitacora
 
 
 class User(commands.Cog):
     def __init__(self, bot: Bitacora):
         self.bot = bot
+
+    @app_commands.command(name='balance')
+    async def balance(self, interaction: discord.Interaction):
+        """Check your coin balance"""
+        user = mongo.User(interaction.guild_id, interaction.user.id)
+        user_info = await user.check_user()
+        balance = user_info.get('balance', 0)
+        await interaction.response.send_message(
+            f'Your balance is {balance} coins', ephemeral=True
+        )
