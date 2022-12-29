8f48b2d6d7764e57a0adef59c152183a33247c97
galind <guillem.yt1@gmail.com>
2022-12-29 13:00:38 +0100

    Added start game embed and code

--- cogs/blackjack/blackjack.py
+++ cogs/blackjack/blackjack.py
@@ -2,7 +2,9 @@ from discord.ext import commands
 from discord import app_commands
 import discord
 import random
+import itertools
 
+from cogs.utils import mongo
 from bot import Bitacora
 
 cards = {
@@ -29,25 +31,34 @@ suits = [
 ]
 
 
+def start_game(name, balance, bet) -> discord.Embed:
+    embed = discord.Embed(
+        title=f'{name}\'s blackjack game', color=Bitacora().color
+    )
+    embed.add_field(name='Coins in the wallet', value=balance, inline=False)
+    embed.add_field(name='Current bet', value=bet, inline=False)
+    return embed
+
+
 class StartGame(discord.ui.View):
     def __init__(self, bot: Bitacora):
         self.bot = bot
         super().__init__(timeout=None)
 
-    @discord.ui.button(label='Reduce bet')
-    async def reduce_bet(
+    @discord.ui.button(label='Start game', style=discord.ButtonStyle.primary)
+    async def start_game(
         self, interaction: discord.Interaction, button: discord.Button
     ):
         pass
 
-    @discord.ui.button(label='Raise bet')
+    @discord.ui.button(label='Raise bet', style=discord.ButtonStyle.success)
     async def raise_bet(
         self, interaction: discord.Interaction, button: discord.Button
     ):
         pass
 
-    @discord.ui.button(label='Start game')
-    async def start_game(
+    @discord.ui.button(label='Lower bet', style=discord.ButtonStyle.danger)
+    async def lower_bet(
         self, interaction: discord.Interaction, button: discord.Button
     ):
         pass
@@ -57,19 +68,24 @@ class Blackjack(commands.Cog):
     def __init__(self, bot: Bitacora) -> None:
         self.bot = bot
 
-    def shuffle_deck(self, deck: list) -> list:
-        random.shuffle(deck)
-        random.shuffle(deck)
-        random.shuffle(deck)
-        return deck
-
-    def get_deck(self) -> list:
-        deck = []
-        for s in suits:
-            for c in cards:
-                deck.append(f'{c} of {s}')
-        return self.shuffle_deck(deck)
+    def start_game(self) -> discord.Embed:
+        embed = discord.Embed()
+        embed.title = f'{self.user.name}\'s blackjack game'
+        return embed
 
     @app_commands.command(name='blackjack')
     async def blackjack(self, interaction: discord.Interaction) -> None:
         """Start a blackjack game"""
+        user = mongo.User(interaction.guild_id, interaction.user.id)
+        user_info = await user.check_user()
+        balance = user_info.get('balance', 0)
+        if balance == 0:
+            return await interaction.response.send_message(
+                'Your wallet is empty, you need coins to play', ephemeral=True
+            )
+
+        embed = start_game(interaction.user.name, balance, 0)
+        view = StartGame(self.bot)
+        await interaction.response.send_message(
+            embed=embed, view=view, ephemeral=True
+        )
